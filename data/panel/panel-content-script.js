976af572046c7ef476092b7d4e67f3c3f54f8c88
Jake <jake@local.dev>
2016-11-28 22:52:48 -0500

    DB to track how many times a fingerprint has been seen in the wild

--- data/panel/panel-content-script.js
+++ data/panel/panel-content-script.js
@@ -1,3 +1,4 @@
-self.port.on('changeInfo', function(certOrg) {
-    document.getElementById("certOrg").innerHTML = certOrg;
+self.port.on('changeInfo', function(data) {
+    document.getElementById("name").innerHTML = data.name;
+    document.getElementById("times_seen").innerHTML = data.times_seen;
 });
