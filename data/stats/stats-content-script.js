b26488de43ead3679425cfba4de268316d363026
Jake <jake@local.dev>
2016-12-11 13:57:36 -0500

    changing stats page

--- data/stats/stats-content-script.js
+++ data/stats/stats-content-script.js
@@ -2,7 +2,7 @@ self.port.on('populateStatsTable', function(data) {
     for (var i = 0; i < data.length; i++) {
         document.getElementById('table_body').innerHTML += "<tr>" +
             "<td>" + data[i].name + "</td>" +
-            "<td>" + data[i].fingerprint + "</td>" +
+            "<td> <textarea readonly rows='4' cols='50'>" + data[i].fingerprint + "</textarea></td>" +
             "<td>" + data[i].times_seen + "</td>" +
             "</tr>";
     }
